87054fe9a9ca2ddbe1e3dee663ade0ab01aabea8
omudavid <omudavid@gmail.com>
2022-12-07 21:57:31 +0100

    Currency Converter UI Completed

---
+++ app/src/main/java/com/hackerman/currencyapp/features/currencyconverter/presentation/CurrencyConverterViewModel.kt
@@ -0,0 +1,8 @@
+package com.hackerman.currencyapp.features.currencyconverter.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class CurrencyConverterViewModel: ViewModel() {
+}
